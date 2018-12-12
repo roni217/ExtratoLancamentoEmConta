258808fc3983813ee3ccd02e4ed7d06667347d8f
Ronaldo Akio Larrubia Yamashita <roni.217@gmail.com>
2018-12-12 13:12:54 -0200

    criado pom e estrutura base

---
+++ extratos/src/main/java/com/lad/extratos/ExtratosApplication.java
@@ -0,0 +1,14 @@
+package com.lad.extratos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtratosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtratosApplication.class, args);
+	}
+
+}
+
